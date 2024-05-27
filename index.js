6eb5bc00feaaa8e62a4761aba9ee0888d5859c04
adiatraa <adiatrahman31@gmail.com>
2024-05-27 20:14:32 +0700

    Create sidebar (onprocess)

---
+++ index.js
@@ -0,0 +1,15 @@
+// let students = ['Adiat', 'Rahman', 'XXXXXX']
+
+// students.push('Cathy')
+// console.log(students)
+
+// for(let i = 0; i < students.length; i++){
+//     console.log(`nama: ${students[i]}`)
+// }
+
+let students = ['John', 'Jane', 'Jenny']
+
+console.log('list of students')
+for (let i = 0; i < 3; i++) { // true
+    console.log(`no. ${i + 1}: ${students[i]}`)
+}
