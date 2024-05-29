a702ba66860234e09254185b4ec4d8bfbd55a247
adiatraa <adiatrahman31@gmail.com>
2024-05-29 20:48:21 +0700

    Test Fetching OpenWeather API

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-// let students = ['Adiat', 'Rahman', 'XXXXXX']
-
-// students.push('Cathy')
-// console.log(students)
-
-// for(let i = 0; i < students.length; i++){
-//     console.log(`nama: ${students[i]}`)
-// }
-
-let students = ['John', 'Jane', 'Jenny']
-
-console.log('list of students')
-for (let i = 0; i < 3; i++) { // true
-    console.log(`no. ${i + 1}: ${students[i]}`)
+//Test Fetch API
+async function fetchData(){
+    let searchCity = 'jakarta'
+    let apiKey = '224409a97cfb66a9475367480e6ac1e2'
+    const responseOpenWeather = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${apiKey}`)
+    const responseOpenWeatherJson = await responseOpenWeather.json()
+    console.log(responseOpenWeatherJson, "<< response Open Weather Json")
 }
+
+fetchData()
