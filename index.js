6f29a510a8c2df0c321e92465088e68ac58b3988
adiatraa <adiatrahman31@gmail.com>
2024-05-30 22:55:12 +0700

    Assign API Value to Navbar

--- index.js
+++ index.js
@@ -7,23 +7,40 @@
 //     console.log(responseOpenWeatherJson, "<< response Open Weather Json")
 // }
 
-function getUserLocation(){
-    navigator.geolocation.getCurrentPosition(
-        fetchOpenMeteoAPI,
-        (error) => {
-            console.error("Error Getting Location: ", error);
-            alert("Error Getting Location: " + error.message);
-        }
+function getUserLocation() {
+  navigator.geolocation.getCurrentPosition(fetchOpenMeteoAPI, (error) => {
+    console.error("Error Getting Location: ", error);
+    alert("Error Getting Location: " + error.message);
+  });
+}
+
+getUserLocation();
+
+async function fetchOpenMeteoAPI(position) {
+  var lat = position?.coords?.latitude;
+  var lon = position?.coords?.longitude;
+
+  document.getElementById("loading").style.display = "block";
+
+  if (lat && lon) {
+    const responseOpenMeteo = await fetch(
+      `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,relative_humidity_2m,is_day,weather_code,wind_speed_10m&hourly=temperature_2m&timezone=Asia%2FBangkok`
     );
+    const responseOpenMeteoJson = await responseOpenMeteo.json();
+    document.getElementById("loading").style.display = "none";
+    console.log(responseOpenMeteoJson, "<< Response Open Meteo Json");
+
+    document.getElementById("temp-sidebar").innerHTML =
+      responseOpenMeteoJson.current.temperature_2m;
+    
+    //Assign API Value to Navbar
+    const responseGeocoding =
+      await fetch(`http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=10&appid=224409a97cfb66a9475367480e6ac1e2
+    `);
+    const responseGeocodingJson = await responseGeocoding.json();
+    document.getElementById("h2-nav-loc-name").innerHTML = responseGeocodingJson[0].name;
+    document.getElementById("p-nav-loc-name").innerHTML = responseGeocodingJson[0].state;
+  }
 }
 
-getUserLocation()
-
-async function fetchOpenMeteoAPI(position){
-    var lat = position.coords.latitude
-    var lon = position.coords.longitude
-
-    const responseOpenMeteo = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,precipitation,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,precipitation_hours&timezone=Asia%2FBangkok`)
-        const responseOpenMeteoJson = await responseOpenMeteo.json()
-        console.log(responseOpenMeteoJson, "<< Response Open Meteo Json")
-}
+fetchOpenMeteoAPI();
